7c54544adfae9fb94e2d8e34054a121299651b72
Trevor Hackett <1577822+yarrgh@users.noreply.github.com>
2021-02-16 21:48:34 -0700

    persist cart state to localstorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
 import { userReducer } from "./user/userSlice";
 import { cartReducer } from "./cart/cartSlice";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    cart: cartReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
